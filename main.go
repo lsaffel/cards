f2bb711f354e21eb8ec953355c2eb7bf820dc882
Lisa <llvvss@icloud.com>
2022-03-27 14:24:05 -0600

    for loops for creating a new deck of cards, values one to four

--- main.go
+++ main.go
@@ -2,12 +2,7 @@ package main
 
 func main(){
 
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()	// this function is in the deck.go file
-}
-
-func newCard() string{		// string here says what the function returns, a string
-	return "Five of Diamonds"
 }
