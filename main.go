988800df5fbd64ce39157c9e46fee2929f5dc715
Lisa <llvvss@icloud.com>
2022-03-27 13:54:42 -0600

    deck type and file added

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
-import "fmt"
-
 func main(){
-	// var card string = "Ace of Spades"
-	// this line is the equivalent to that line and this shorter version
-	// is more often used to declare a variable: 
-	// card := "Ace of Spades"
 
-	card := newCard()
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
 
-	fmt.Println(card)
+	cards.print()	// this function is in the deck.go file
 }
 
 func newCard() string{		// string here says what the function returns, a string
