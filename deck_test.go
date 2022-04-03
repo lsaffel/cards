cd0a6517a5c3c53ecfb846064ff23b95ea187e45
Lisa <llvvss@icloud.com>
2022-04-02 20:08:03 -0600

    added some tests to test function

--- deck_test.go
+++ deck_test.go
@@ -1,2 +1,22 @@
 package main
 
+import "testing"
+
+// t here is the test handler
+func TestNewDeck (t *testing.T) {
+	d := newDeck()
+
+	// we have 4 suits and 4 values, so there should be 16 total cards
+	if len(d) != 16 {
+		// this  works. Apparently, the f represents formatting:
+		t.Errorf("Expected de	ck length of 16, but got %v", len(d))
+	}			
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0] )
+	}
+		
+	if d[len(d) - 1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d) - 1])
+	}
+}
