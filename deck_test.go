4eb709a35116f34f7afb9cc3a4806c1f31224df9
Lisa <llvvss@icloud.com>
2022-04-04 22:06:34 -0600

    added card values to make a full 52 card deck, adjusted tests for the full 52 cards - works

--- deck_test.go
+++ deck_test.go
@@ -9,19 +9,21 @@ import (
 func TestNewDeck (t *testing.T) {
 	d := newDeck()
 
-	// we have 4 suits and 4 values, so there should be 16 total cards
-	if len(d) != 16 {
-		// this  works. Apparently, the f represents formatting:
-		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	// we have 4 suits and 13 values, so there should be 52 total cards
+	if len(d) != 52 {
+		// this  works. The f represents formatting:
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}			
 
+	// check the first card in the deck to make sure it's correct
 	if d[0] != "Ace of Spades" {
 		// %v injects the value into the string
 		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0] )
 	}
-		
-	if d[len(d) - 1] != "Four of Clubs" {
-		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d) - 1])
+	
+	// check the last card in the deck
+	if d[len(d) - 1] != "King of Clubs" {
+		t.Errorf("Expected last card of King of Clubs, but got %v", d[len(d) - 1])
 	}
 }
 
@@ -34,8 +36,9 @@ func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
 
 	loadedDeck := newDeckFromFile("_decktesting")
 
-	if len(loadedDeck) != 16 {
-		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	// make sure the deck has the correct number of cards in it, 52
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
 	}
 
 	os.Remove("_decktesting")
